43910cf1774a7bf8d4c17a02b7200bf5aea98b7d
Drzid <dzz@pop-os.localdomain>
2023-05-28 20:41:01 +0700

    first commit

---
+++ components/Result.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Result = ({state}) => {
+    const {billAmount, tipPercentage, noOfPerson} = state;
+    const totalTip = Number((state.billAmount * tipPercentage)/100).toFixed(2);
+return(
+<div className="tip-result">
+<div>
+    <p className="label">Total Tip</p>{" "}
+    <p className="result">{`$ ${totalTip}`}</p>
+</div>
+
+<div>
+<p className="label">Tip Per Person</p>{" "}
+<p className="result">{`$ ${Number(totalTip / noOfPerson).toFixed(2)}`}</p>
+</div>
+</div>
+);
+};
+
+export default Result;
